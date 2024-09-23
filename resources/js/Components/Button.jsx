c1e72f674c0855ae78ca7253f2af2db6c3b13b3c
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-09-23 21:12:02 +0700

    component

---
+++ resources/js/Components/Button.jsx
@@ -0,0 +1,20 @@
+export default function Button({
+    className = "",
+    disabled,
+    children,
+    ...props
+}) {
+    return (
+        <button
+            {...props}
+            className={
+                `inline-flex items-center px-2 py-1 border border-transparent rounded-none font-semibold text-xs text-white tracking-widest hover:bg-opacity-80 focus:outline-none focus:ring-2 focus:ring-offset-2 transition ease-in-out duration-150 ${
+                    disabled && "opacity-25"
+                } ` + className
+            }
+            disabled={disabled}
+        >
+            {children}
+        </button>
+    );
+}
---
+++ resources/js/Components/LinkButton.jsx
@@ -0,0 +1,23 @@
+import { Link } from "@inertiajs/react";
+
+export default function LinkButton({
+    active = false,
+    className = "",
+    children,
+    ...props
+}) {
+    return (
+        <Link
+            {...props}
+            className={
+                "inline-flex items-center px-4 py-2 rounded-none border border-transparent text-sm font-medium transition ease-in-out duration-150 focus:outline-none tracking-widest focus:ring-2" +
+                (active
+                    ? "border-indigo-400 text-gray-200 focus:border-slate-200"
+                    : "border-transparent text-gray-500 hover:text-slate-200 hover:border-slate-200 focus:text-slate-500 focus:border-gray-300 ") +
+                className
+            }
+        >
+            {children}
+        </Link>
+    );
+}
