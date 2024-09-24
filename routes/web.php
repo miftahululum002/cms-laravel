76afa22a53ab33b598d9f322037b13498af0ca96
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-09-24 18:28:17 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,34 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\PostHomeController;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// })->name('index');
+Route::name('home.')->group(function () {
+    Route::controller(HomeController::class)->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::get('/contact', 'contact')->name('contact');
+        Route::get('/about', 'about')->name('about');
+    });
+    Route::name('blog.')->group(function () {
+        Route::prefix('blog')->group(function () {
+            Route::controller(PostHomeController::class)->group(function () {
+                Route::get('/', 'index')->name('index');
+                Route::get('/categories/{category}', 'categories')->name('categories');
+            });
+        });
+    });
 });
 
 // Route::get('/dashboard', function () {
@@ -26,18 +41,22 @@ Route::middleware(['auth'])->group(function () {
             Route::get('/', 'index')->name('dashboard');
         });
         Route::name('dashboard.')->group(function () {
-
             Route::prefix('posts')->group(function () {
                 Route::name('posts.')->group(function () {
                     Route::controller(PostController::class)->group(function () {
                         Route::get('/', 'index')->name('index');
+                        Route::get('create', 'create')->name('create');
+                        Route::get('edit/{post}', 'edit')->name('edit');
+                        Route::get('show/{post}', 'show')->name('show');
+                        Route::post('/store', 'store')->name('store');
+                        Route::patch('/update', 'update')->name('update');
+                        Route::delete('/delete', 'destroy')->name('destroy');
                     });
                 });
             });
         });
     });
-});
-Route::middleware('auth')->group(function () {
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
