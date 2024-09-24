09fa3337911d5f08dc033642114802f910e91b44
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-09-24 18:30:50 +0700

    app blade

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,13 +4,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
     <!-- Fonts -->
     <!-- <link rel="preconnect" href="https://fonts.bunny.net"> -->
     <!-- <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" /> -->
-
     <!-- Scripts -->
     @routes
     @viteReactRefresh
@@ -20,6 +17,7 @@
 
 <body class="font-sans antialiased">
     @inertia
+    <script src="{{asset('js')}}/script.js"></script>
 </body>
 
 </html>
