0b737b86f48b77c96f0922fac7205bd141735aa2
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-09-24 18:28:58 +0700

    base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,14 +7,26 @@ use Inertia\Inertia;
 abstract class Controller
 {
     protected $directory = null;
+    protected $isHome = false;
     protected $data = [];
 
     protected function render($filename)
     {
+        $this->setDataHook();
         return Inertia::render($this->getView($filename), $this->data);
     }
+
     protected function getView($filename)
     {
         return !empty($this->directory) ? $this->directory . '/' . $filename : $filename;
+    }
+
+    private function setDataHook()
+    {
+        if ($this->isHome) {
+            $this->data['auth'] = auth()->user();
+            $this->data['home_categories'] = getCategories();
+        }
+        $this->data['author'] = config('miftahululum.author');
     }
 }
