a7266d2c7e43bc9b25794a8269dd9ef2c18c9fff
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-09-24 18:30:31 +0700

    dashboard page

--- resources/js/Pages/Dashboard/Index.jsx
+++ resources/js/Pages/Dashboard/Index.jsx
@@ -1,17 +1,25 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head, usePage } from "@inertiajs/react";
 
-export default function Dashboard() {
+export default function Dashboard({ title }) {
+    const user = usePage().props.auth.user;
     return (
         <AuthenticatedLayout
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                    {title}
+                </h2>
+            }
         >
-            <Head title="Dashboard" />
-
+            <Head title={title} />
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
+                        <div className="p-6 text-gray-900">
+                            Selamat Datang Kembali{" "}
+                            <span className="font-bold">{user.name}</span>
+                            <p>Semoga harimu menyenangkan</p>
+                        </div>
                     </div>
                 </div>
             </div>
