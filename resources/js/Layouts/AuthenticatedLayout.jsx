a69586a848feb3537d1b9c13488f2baae0cb81d5
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-09-24 18:29:58 +0700

    layouts

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -7,7 +7,7 @@ import { Link, usePage } from "@inertiajs/react";
 
 export default function Authenticated({ header, children }) {
     const user = usePage().props.auth.user;
-
+    const appName = import.meta.env.VITE_APP_NAME || "Laravel";
     const [showingNavigationDropdown, setShowingNavigationDropdown] =
         useState(false);
     const menus = [
@@ -21,8 +21,9 @@ export default function Authenticated({ header, children }) {
                     <div className="flex justify-between h-16">
                         <div className="flex">
                             <div className="shrink-0 flex items-center">
-                                <Link href="/">
-                                    <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
+                                <Link href={route("dashboard")}>
+                                    {/* <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" /> */}
+                                    <span>{appName}</span>
                                 </Link>
                             </div>
 
