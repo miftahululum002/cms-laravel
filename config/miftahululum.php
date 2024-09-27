7b9cffd4b072c9b4d736efb4cd52c3a99245b87d
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-09-27 16:40:26 +0700

    config

--- config/miftahululum.php
+++ config/miftahululum.php
@@ -4,6 +4,7 @@ return [
     'author' => (object)[
         'name' => 'Miftahul Ulum',
         'address' => 'Jl. Panglima Sudirman RT 48 RW 14 Desa Codo Kec. Wajak Kab. Malang',
-        'email' => 'ulumiftahul06@gmail.com'
+        'email' => 'ulumiftahul06@gmail.com',
+        'github' => 'https://miftahululum002.github.io'
     ],
 ];
