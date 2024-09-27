512d3a57731aae252f1634b0cf7848bfa9024586
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-09-27 16:41:01 +0700

    crud category

---
+++ resources/js/Pages/Category/Edit.jsx
@@ -0,0 +1,67 @@
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import InputError from "@/Components/InputError";
+import InputLabel from "@/Components/InputLabel";
+import PrimaryButton from "@/Components/PrimaryButton";
+import TextInput from "@/Components/TextInput";
+import { Head, Link, useForm } from "@inertiajs/react";
+export default function Edt({ status, title, category }) {
+    const {
+        data,
+        setData,
+        post: update,
+        processing,
+        errors,
+    } = useForm({
+        name: category.name,
+        id: category.id,
+        _method: "PATCH",
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        update(route("dashboard.categories.update"), {
+            // onFinish: () => reset("content"),
+        });
+    };
+    return (
+        <AuthenticatedLayout
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                    {title}
+                </h2>
+            }
+        >
+            <Head title={title} />
+            {status && (
+                <div className="mb-4 font-medium text-sm text-green-600">
+                    {status}
+                </div>
+            )}
+            <div className="w-1/2 mx-auto my-10 bg-white p-2">
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                        <InputLabel htmlFor="name" value="Judul" />
+                        <TextInput
+                            id="name"
+                            type="text"
+                            name="name"
+                            value={data.name}
+                            className="mt-1 block w-full"
+                            autoComplete="title"
+                            placeholder="Judul"
+                            isFocused={true}
+                            onChange={(e) => setData("name", e.target.value)}
+                        />
+                        <InputError message={errors.name} className="mt-2" />
+                    </div>
+
+                    <div className="flex items-center justify-end mt-4">
+                        <PrimaryButton className="ms-4" disabled={processing}>
+                            Simpan
+                        </PrimaryButton>
+                    </div>
+                </form>
+            </div>
+        </AuthenticatedLayout>
+    );
+}
---
+++ resources/js/Pages/Category/Index.jsx
@@ -0,0 +1,223 @@
+import DangerButton from "@/Components/DangerButton";
+import PrimaryButton from "@/Components/PrimaryButton";
+import LinkButton from "@/Components/LinkButton";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import SecondaryButton from "@/Components/SecondaryButton";
+import { Head, Link, useForm } from "@inertiajs/react";
+import { useState } from "react";
+import Modal from "@/Components/Modal";
+import TextInput from "@/Components/TextInput";
+
+export default function Categories({ categories, title }) {
+    const [confirmingCategoryDeletion, setConfirmingCategoryDeletion] =
+        useState(false);
+
+    const [confirmingCategoryActivation, setConfirmingCategoryActivation] =
+        useState(false);
+    const {
+        data,
+        setData,
+        delete: destroy,
+        patch,
+        processing,
+        reset,
+        clearErrors,
+    } = useForm();
+
+    const confirmCategoryDeletion = (id) => {
+        setData("id", id);
+        setConfirmingCategoryDeletion(true);
+    };
+    const confirmCategoryActivation = (id) => {
+        setData("id", id);
+        setConfirmingCategoryActivation(true);
+    };
+
+    const closeModal = () => {
+        setConfirmingCategoryDeletion(false);
+        clearErrors();
+        reset();
+    };
+    const closeModalActivation = () => {
+        setConfirmingCategoryActivation(false);
+        clearErrors();
+        reset();
+    };
+
+    const handleCategoryDelete = (e) => {
+        e.preventDefault();
+        destroy(route("dashboard.categories.destroy"), {
+            preserveScroll: true,
+            onSuccess: () => closeModal(),
+            onFinish: () => reset(),
+        });
+    };
+    const handleCategoryActivate = (e) => {
+        e.preventDefault();
+        patch(route("dashboard.categories.activate"), {
+            preserveScroll: true,
+            onSuccess: () => closeModalActivation(),
+            onFinish: () => reset(),
+        });
+    };
+    return (
+        <AuthenticatedLayout
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                    {title}
+                </h2>
+            }
+        >
+            <Head title={title} />
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-none p-3">
+                        <div>
+                            <LinkButton
+                                href={route("dashboard.categories.create")}
+                                className="bg-primary mb-2 text-white"
+                            >
+                                Tambah
+                            </LinkButton>
+                        </div>
+                        <table className="table-auto w-full border-collapse border border-slate-400">
+                            <thead className="border border-slate-300 my-4">
+                                <tr>
+                                    <th className="w-auto px-2 border border-slate-400">
+                                        No
+                                    </th>
+                                    <th className="w-auto px-2 border border-slate-400 text-left">
+                                        Nama
+                                    </th>
+
+                                    <th className="w-auto px-2 border border-slate-400 text-left">
+                                        Slug
+                                    </th>
+
+                                    <th className="border border-slate-400 text-center">
+                                        Opsi
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {categories.map((category, index) => (
+                                    <tr key={index}>
+                                        <td className="w-auto px-2 border border-slate-400 text-center">
+                                            {index + 1}
+                                        </td>
+                                        <td className="w-auto px-2 border border-slate-400">
+                                            {category.name}
+                                        </td>
+
+                                        <td className="w-auto px-2 border border-slate-400">
+                                            {category.slug}
+                                        </td>
+
+                                        <td className="px-2 border border-slate-400 text-center">
+                                            {category.is_deleted == "1" ? (
+                                                <>
+                                                    <PrimaryButton
+                                                        onClick={(e) =>
+                                                            confirmCategoryActivation(
+                                                                category.id
+                                                            )
+                                                        }
+                                                    >
+                                                        Kembalikan
+                                                    </PrimaryButton>
+                                                </>
+                                            ) : (
+                                                <>
+                                                    <Link
+                                                        href={
+                                                            route(
+                                                                "home.blog.index"
+                                                            ) +
+                                                            "?category=" +
+                                                            category.slug
+                                                        }
+                                                        className="inline-flex text-primary font-bold justify-center items-center w-full"
+                                                    >
+                                                        Lihat
+                                                    </Link>
+                                                    <Link
+                                                        href={route(
+                                                            "dashboard.categories.edit",
+                                                            category.id
+                                                        )}
+                                                        className="inline-flex items-center px-2 py-1 mb-2 bg-primary border border-transparent rounded-none font-semibold text-xs text-white uppercase tracking-widest hover:bg-opacity-7 focus:bg-gray-700 active:bg-secondary focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
+                                                    >
+                                                        Edit
+                                                    </Link>
+                                                    <DangerButton
+                                                        onClick={(e) =>
+                                                            confirmCategoryDeletion(
+                                                                category.id
+                                                            )
+                                                        }
+                                                    >
+                                                        Hapus
+                                                    </DangerButton>
+                                                </>
+                                            )}
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <Modal show={confirmingCategoryDeletion} onClose={closeModal}>
+                <form onSubmit={handleCategoryDelete} className="p-6">
+                    <TextInput type="hidden" name="id" id="category_id" />
+                    <h2 className="text-lg font-medium text-gray-900">
+                        Are you sure you want to delete this category?
+                    </h2>
+                    <p className="mt-1 text-sm text-gray-600">
+                        Once category is deleted, all of its resources and data
+                        will be permanently deleted.
+                    </p>
+                    <div className="mt-6 flex justify-end">
+                        <SecondaryButton
+                            className="rounded-none"
+                            onClick={closeModal}
+                        >
+                            Batal
+                        </SecondaryButton>
+
+                        <DangerButton className="ms-3" disabled={processing}>
+                            Hapus
+                        </DangerButton>
+                    </div>
+                </form>
+            </Modal>
+            <Modal
+                show={confirmingCategoryActivation}
+                onClose={closeModalActivation}
+            >
+                <form onSubmit={handleCategoryActivate} className="p-6">
+                    <TextInput type="hidden" name="id" id="post_id" />
+                    <h2 className="text-lg font-medium text-gray-900">
+                        Apakah Anda yakin mengaktifkan kembali kategori ini?
+                    </h2>
+                    <p className="mt-1 text-sm text-gray-600">
+                        Setelah kategori diaktifkan kembali, semua sumber daya
+                        dan data akan diaktifkan kembali.
+                    </p>
+                    <div className="mt-6 flex justify-end">
+                        <SecondaryButton
+                            className="rounded-none"
+                            onClick={closeModalActivation}
+                        >
+                            Batal
+                        </SecondaryButton>
+                        <PrimaryButton className="ms-3" disabled={processing}>
+                            Kembalikan
+                        </PrimaryButton>
+                    </div>
+                </form>
+            </Modal>
+        </AuthenticatedLayout>
+    );
+}
