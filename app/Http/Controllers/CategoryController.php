4a383910459662fafc3877ba28abbf766216016e
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-09-27 16:39:34 +0700

    category controller

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CategoryStoreRequest;
+use App\Http\Requests\CategoryUpdateRequest;
+use App\Models\Category;
+use Exception;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    protected $directory = 'Category';
+
+    public function index()
+    {
+        $this->data['title'] = 'Kategori Tulisan';
+        $this->data['categories'] = getAllCategories();
+        return $this->render('Index');
+    }
+
+    public function create()
+    {
+        $this->data['title'] = 'Tambah Kategori';
+        return $this->render('Create');
+    }
+
+    public function store(CategoryStoreRequest $request)
+    {
+        $input = $request->validated();
+        $data = $input;
+        $data['slug'] = getSlug($data['name']);
+        try {
+            createCategory($data);
+            return redirect(route('dashboard.categories.index'));
+        } catch (Exception $e) {
+            return redirect(route('dashboard.categories.create'))->withErrors($e->getMessage());
+        }
+    }
+
+    public function edit(Category $category)
+    {
+        $this->data['category'] = $category;
+        $this->data['title'] = "Edit Kategori";
+        return $this->render('Edit');
+    }
+
+    public function update(CategoryUpdateRequest $request)
+    {
+        $input = $request->validated();
+        $data = $input;
+        $data['slug'] = getSlug($data['name']);
+        $id = $data['id'];
+        try {
+            updateCategory($id, $data);
+            return redirect(route('dashboard.categories.index'));
+        } catch (Exception $e) {
+            return redirect(route('dashboard.categories.edit', [$id]))->withErrors($e->getMessage());
+        }
+    }
+
+    public function destroy(Request $request)
+    {
+        $id = $request->id;
+        try {
+            deleteCategory($id);
+            return redirect(route('dashboard.categories.index'));
+        } catch (Exception $e) {
+            return redirect(route('dashboard.categories.index'))->withErrors($e->getMessage());
+        }
+    }
+
+    public function activate(Request $request)
+    {
+        $id = $request->id;
+        try {
+            updateCategory($id, ['is_deleted' => '0', 'is_restored' => '1']);
+            return redirect(route('dashboard.categories.index'));
+        } catch (Exception $e) {
+            return redirect(route('dashboard.categories.index'))->withErrors($e->getMessage());
+        }
+    }
+}
