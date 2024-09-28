a88d821e218b579bcfeb7e741ba66af83a744dc1
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-09-29 06:53:19 +0700

    activity logs

---
+++ database/migrations/2024_09_28_221237_create_activity_logs_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('activity_logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('activity');
+            $table->integer('user_id');
+            $table->text('data')->comment('Data Payload in json')->nullable();
+            $table->string('page_url')->nullable();
+            $table->string('ip_address')->nullable();
+            $table->string('user_agent')->nullable();
+            $table->timestamp('created_at')->useCurrent();
+            $table->integer('created_by')->nullable();
+            $table->timestamp('updated_at')->nullable()->useCurrentOnUpdate();
+            $table->integer('updated_by')->nullable();
+            $table->enum('is_deleted', ['1', '0'])->default('0');
+            $table->timestamp('deleted_at')->nullable();
+            $table->integer('deleted_by')->nullable();
+            $table->enum('is_restored', ['1', '0'])->default('0');
+            $table->timestamp('restored_at')->nullable();
+            $table->integer('restored_by')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('activity_logs');
+    }
+};
