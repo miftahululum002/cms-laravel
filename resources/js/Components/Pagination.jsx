3bf731a5a64295c12d94abc1b1acceab123ecf75
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-09-28 20:39:25 +0700

    pagination components

---
+++ resources/js/Components/Pagination.jsx
@@ -0,0 +1,42 @@
+import { Link } from "@inertiajs/react";
+
+export default function Pagination({ links }) {
+    function getClassName(active) {
+        if (active) {
+            return "mr-1 mb-1 px-4 py-3 text-sm leading-4 border rounded hover:bg-white focus:border-primary focus:text-primary bg-primary text-white";
+        } else {
+            return "mr-1 mb-1 px-4 py-3 text-sm leading-4 border rounded hover:bg-white focus:border-primary focus:text-primary";
+        }
+    }
+
+    return (
+        links.length > 3 && (
+            <div className="mb-2 mt-2 flex justify-center">
+                <div className="flex flex-wrap">
+                    {links.map((link, key) =>
+                        link.url === null ? (
+                            <div
+                                id="pagination-prev"
+                                key={key}
+                                className="mr-1 mb-1 px-4 py-3 text-sm leading-4 border rounded"
+                            >
+                                {link.label}
+                            </div>
+                        ) : (
+                            <Link
+                                id="pagination-next"
+                                key={key}
+                                className={
+                                    getClassName(link.active) + ` text-primary`
+                                }
+                                href={link.url}
+                            >
+                                {link.label}
+                            </Link>
+                        )
+                    )}
+                </div>
+            </div>
+        )
+    );
+}
