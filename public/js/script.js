aa8b4c8b3c84f808f2e2715bc011b5fb25a1cc63
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-09-28 20:39:01 +0700

    handle

--- public/js/script.js
+++ public/js/script.js
@@ -3,14 +3,19 @@ window.onscroll = function () {
     const header = document.querySelector("header");
     const fixedNav = header.offsetTop;
     const toTop = document.querySelector("#to-top");
-
-    if (window.pageYOffset > fixedNav) {
-        header.classList.add("navbar-fixed");
-        toTop.classList.remove("hidden");
-        toTop.classList.add("flex");
-    } else {
-        header.classList.remove("navbar-fixed");
-        toTop.classList.remove("flex");
-        toTop.classList.add("hidden");
+    if (toTop && header) {
+        if (window.pageYOffset > fixedNav) {
+            header.classList.add("navbar-fixed");
+            toTop.classList.remove("hidden");
+            toTop.classList.add("flex");
+        } else {
+            header.classList.remove("navbar-fixed");
+            toTop.classList.remove("flex");
+            toTop.classList.add("hidden");
+        }
     }
 };
+
+// const paginationPrev = document.getElementById("pagination-prev");
+// const paginationNext = document.getElementById("pagination-next");
+// console.log(paginationPrev);
