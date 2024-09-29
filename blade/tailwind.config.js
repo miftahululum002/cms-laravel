7b7a188defb3f15d47343e78778674e2fb2b9509
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-09-29 12:49:30 +0700

    tailwind config

--- blade/tailwind.config.js
+++ blade/tailwind.config.js
@@ -1,18 +1,30 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
+    darkMode: "class",
     theme: {
+        container: {
+            center: true,
+            padding: "16px",
+        },
         extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            // fontFamily: {
+            //     sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            // },
+            colors: {
+                primary: "#06b6d4",
+                secondary: "#64748b",
+                dark: "#164e63",
+            },
+            screens: {
+                "2xl": "1320px",
             },
         },
     },
