3fa96adf9ec5aef14d6c4b2ff8dcaebeb8e728b2
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-09-29 12:50:16 +0700

    home page

--- blade/app/Http/Controllers/HomeController.php
+++ blade/app/Http/Controllers/HomeController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    protected $directory = 'Home';
+    protected $directory = 'home';
     protected $isHome = true;
 
     public function index()
@@ -14,18 +14,18 @@ class HomeController extends Controller
         $this->data['title'] = 'Beranda';
         $posts = getLatestPost(3);
         $this->data['posts'] = $posts;
-        return $this->render('Index');
+        return $this->render('index');
     }
 
     public function about()
     {
         $this->data['title'] = 'Tentang Kami';
-        return $this->render('About');
+        return $this->render('about');
     }
 
     public function contact()
     {
         $this->data['title'] = 'Hubungi Kami';
-        return $this->render('Contact');
+        return $this->render('contact');
     }
 }
